db24e840ac87ae3a84ea2aea0de39d1534881039
fmccl <xfinnbar@gmail.com>
2024-08-05 16:51:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+import repl from "./web_repl.js"
+
+repl.setup();
+
+console.log(repl.run("2+2"))
---
+++ web_repl.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function setup(): void;
+/**
+* @param {string} code
+* @returns {string}
+*/
+export function run(code: string): string;
